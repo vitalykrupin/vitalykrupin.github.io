79b51ab7099404d31b36173390814cc494170d14
Vitaly Krupin <vitalykrupin@gmail.com>
2019-06-25 10:03:39 +0500

    some

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 const input = document.querySelector('.autocomplete__input');
 const citiesList = document.querySelector('.autocomplete__drop-down');
 const city = document.querySelector('.autocomplete__drop-down li');
-// const xhr = new XMLHttpRequest;
-// const URL = 'kladr.json';
-// xhr.open('GET', URL, true);
-// xhr.send();
-
-
-const URL = '/json/kladr.json';
 
 const inputHandler = (evt) => {
   if (evt.target.value) {
@@ -17,6 +10,6 @@ const inputHandler = (evt) => {
 
 input.addEventListener('input', inputHandler);
 
-fetch('/kladr.json')  
+fetch('./kladr.json')  
   .then(res => res.json())
   .then(data => console.log(data))
