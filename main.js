300109f4417cbb85968b2b28f8e4fdca1c98cddd
Vitaly Krupin <vitalykrupin@gmail.com>
2019-06-27 08:28:00 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-const input = document.querySelector('.autocomplete__input');
-const citiesList = document.querySelector('.autocomplete__drop-down');
-const city = document.querySelector('.autocomplete__drop-down li');
+const init = () => {
 
-const inputHandler = (evt) => {
-  if (evt.target.value) {
-    console.log(evt.target.value);
-  }
-}
+  const input = document.querySelector('.autocomplete__input');
+  const dropDown = document.querySelector('.autocomplete__drop-down');
+  const cities = window.cities;
 
-input.addEventListener('input', inputHandler);
+  const search = (inputStr, data) => {
+    if (inputStr) {
+      return data.filter((item) => {
+        return item.City.toLowerCase().indexOf(inputStr.toLowerCase()) === 0;
+      })
+    }
+    unmountDropDown();
+  };
 
-fetch('./cities.json')  
-  .then(res => res.json())
-  .then(data => console.log(data))
+  const unmountDropDown = () => dropDown.innerHTML = ``;
+
+  input.addEventListener('input', (evt) => {
+    const dropDownData = search(evt.target.value, cities);
+    mountDropDown(dropDownData);
+  });
+
+  dropDown.addEventListener('click', (evt) => {
+    input.value = evt.target.innerHTML;
+    unmountDropDown();
+  })
+
+  mountDropDown = (data) => {
+    unmountDropDown();
+
+    const autocompleteList = `
+      <ul class="autocomplete__list">
+        ${data
+          .slice(0, 14)
+          .map((item) => `<li class="autocomplete__list-item">${item.City}</li>`)
+          .sort()
+          .join(``)
+        }
+      </ul>
+    `;
+
+    if (data.length) {
+      return dropDown.innerHTML = autocompleteList;
+    }  
+  };
+};
+
+init();
